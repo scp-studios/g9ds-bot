0bc0012dfd2580546bc4b41cd2cab9ff8017f5f0
can pooper <domesticatedhuman9999@gmail.com>
2022-01-21 10:57:20 -0500

    set up some intents and stuff

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+import discord
+from discord.ext import commands
+
+intents = discord.Intents(guilds=True, members=True, bans=True, emojis=True, voice_states=True, messages=True, reactions=True)
+allowed_mentions = discord.AllowedMentions(roles=False, everyone=False, users=True)
+bot = commands.Bot(command_prefix="", intents=intents, allowed_mentions=allowed_mentions, help_command=None)
+
 print("Hello worl")
