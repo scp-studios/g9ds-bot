cf4d2feccaea00b0f42a4b564ee7c207b91fc59c
can pooper <domesticatedhuman9999@gmail.com>
2022-01-21 11:25:32 -0500

    add ban command and other fun things

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import discord
 import time
 from discord.ext import commands
+from discord.ext.commands import has_permissions
 
 intents = discord.Intents(guilds=True, members=True, bans=True, emojis=True, voice_states=True, messages=True, reactions=True)
 allowed_mentions = discord.AllowedMentions(roles=False, everyone=False, users=True)
@@ -9,6 +10,15 @@ bot = commands.Bot(command_prefix="g9 ", intents=intents, allowed_mentions=allow
 @bot.command(name="ping", aliases=["latency"])
 async def ping(ctx):
     await ctx.reply(f"Woah, the ping is `{str(round(bot.latency * 1000))}ms`.")
+
+@bot.command(name="ban", aliases=["yeet"])
+@has_permissions(ban_members=True)
+async def ban(ctx, member: discord.Member, *, reason = None):
+    try:
+        await member.ban(reason = reason)
+        await ctx.reply(f"yikes {member} was banned\nreason: {reason}")
+    except discord.errors.Forbidden:
+        await ctx.reply("i dont have permissions to ban that user")
 
 @bot.event
 async def on_ready():
