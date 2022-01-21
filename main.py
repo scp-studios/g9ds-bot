e3165cb0abda00683565ec062a5e92430db36615
can pooper <domesticatedhuman9999@gmail.com>
2022-01-21 10:59:11 -0500

    change prefix

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from discord.ext import commands
 
 intents = discord.Intents(guilds=True, members=True, bans=True, emojis=True, voice_states=True, messages=True, reactions=True)
 allowed_mentions = discord.AllowedMentions(roles=False, everyone=False, users=True)
-bot = commands.Bot(command_prefix="", intents=intents, allowed_mentions=allowed_mentions, help_command=None)
+bot = commands.Bot(command_prefix="g9 ", intents=intents, allowed_mentions=allowed_mentions, help_command=None)
 
 print("Hello worl")
