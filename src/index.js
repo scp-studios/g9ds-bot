36369ea3401c54019137d7351ca815eb4ad9450b
Hello56721 <Hello56721@github.com>
2022-01-21 13:16:19 -0500

    Created basic Discord bot structure.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
-console.log("Hello, World!");
+const {
+    Client,
+    Intents
+} = require("discord.js")
+
+const {
+    token
+} = require("./token.json")
+
+const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
+
+bot.once("ready", () => {
+    console.log("The bot has logged in.")
+})
+
+bot.login(token)
