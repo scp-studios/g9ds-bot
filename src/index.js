2467a3fe4daf1594c8505cd68da9e5c3e401f07f
Hello56721 <Hello56721@github.com>
2022-01-21 13:35:24 -0500

    Deploy commands each time at launch.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
-const {
-    Client,
-    Intents
-} = require("discord.js")
+const Discord = require("discord.js")
+const DeployCommands = require("./deploy-commands")
+const Token = require("./token.json")
 
-const {
-    token
-} = require("./token.json")
+DeployCommands.deployCommands()
 
-const bot = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]})
+const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.GUILD_MESSAGES]})
 
 bot.once("ready", () => {
     console.log("The bot has logged in.")
 })
 
-bot.login(token)
+bot.on("interactionCreate", async (interaction) => {
+    if (!interaction.isCommand()) {
+        return;
+    }
+    
+    if (interaction.commandName == "ping")
+    {
+        interaction.reply("Super idol de xiao rong")
+    }
+})
+
+bot.login(Token.token)
