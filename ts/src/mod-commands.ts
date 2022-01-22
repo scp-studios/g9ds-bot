65c1e8a6d8aff966e0881fc05274bd466d59f4a4
Hello56721 <Hello56721@github.com>
2022-01-22 16:57:24 -0500

    Added checking for the moderation commands.

--- ts/src/mod-commands.ts
+++ ts/src/mod-commands.ts
@@ -1,20 +1,37 @@
-import { APIInteractionDataResolvedGuildMember, APIRole } from "discord-api-types";
-import { CommandInteraction, Guild, GuildMember, Role, User } from "discord.js";
+import { 
+    CommandInteraction,
+    GuildMember,
+    Permissions
+} from "discord.js";
 
 export function kick(interaction: CommandInteraction) {
+    if (!(interaction.memberPermissions?.has(Permissions.FLAGS.KICK_MEMBERS))) {
+        interaction.reply("u dont have kick permissions bozo :joy_cat: :joy_cat: :joy_cat:")
+        return;
+    }
+    
     const member: GuildMember  = interaction.options.getMentionable("member", true) as GuildMember
     const reason: string = interaction.options.getString("reason", false) as string
     
-    member.kick(reason)
-    
-    interaction.reply({ content: `Successfully kicked ${member}.`, ephemeral: true })
+    member.kick(reason).then(() => {
+        interaction.reply({ content: `Successfully kicked ${member}.`, ephemeral: true })
+    }).catch((error) => {
+        interaction.reply({ content: `Failed to kick ${member}: ${error}`, ephemeral: true })
+    })
 }
 
 export function ban(interaction: CommandInteraction) {
+    if (!(interaction.memberPermissions?.has(Permissions.FLAGS.BAN_MEMBERS))) {
+        interaction.reply("u dont have kick permissions bozo :joy_cat: :joy_cat: :joy_cat:")
+        return;
+    }
+    
     const member: GuildMember  = interaction.options.getMentionable("member", true) as GuildMember
     const reason: string = interaction.options.getString("reason", false) as string
     
-    member.ban({ reason: reason })
-    
-    interaction.reply({ content: `Successfully banned ${member}.`, ephemeral: true })
+    member.ban({ reason: reason }).then(() => {
+        interaction.reply({ content: `Successfully banned ${member}.`, ephemeral: true })
+    }).catch((error) => {
+        interaction.reply({ content: `Failed to ban ${member}: ${error}`, ephemeral: true })
+    })
 }
