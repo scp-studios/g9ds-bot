5b5167e772177520d428d2275aad0e18d851e04e
can pooper <domesticatedhuman9999@gmail.com>
2022-01-24 11:23:07 -0500

    added joy cat

--- src/main.py
+++ src/main.py
@@ -108,7 +108,7 @@ async def on_ready():
 async def on_reaction_add(reaction, user):
     if reaction.message.id == banmsg:
         await user.ban(reason = "Asked for it")
-        await reaction.message.channel.send(f"{user} has been banned")
+        await reaction.message.channel.send(f"{user} has been banned! :joy_cat:")
         
 
 token = open("../token.txt", "r").read()
